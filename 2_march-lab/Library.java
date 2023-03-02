23c0fadb7b15a61c372b5edd29c235e942d52d46
GAUTAM CHAUDHARY <122784792+letscodewithgautam@users.noreply.github.com>
2023-03-02 18:43:20 +0530

    Add files via upload

---
+++ 2_march-lab/Library.java
@@ -0,0 +1,25 @@
+package mar_02_23;
+
+public class Library {
+	
+	private int bookId;
+	private String bookName,bookAuthor;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+}
